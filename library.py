660150e20303f24bd7a2369e20dd49c31b7040d3
Karma Woeser <111936122+kwoeser@users.noreply.github.com>
2025-04-30 21:45:22 -0700

    Update library.py

--- library.py
+++ library.py
@@ -5,7 +5,8 @@ import types
 from typing import Dict, Any, Optional, Union, List, Set, Hashable, Literal, Tuple, Self, Iterable
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
-import sklearn
+from sklearn.impute import KNNImputer
+import sklearnv
 sklearn.set_config(transform_output="pandas")  #says pass pandas tables through pipeline instead of numpy matrices
 import warnings
 
