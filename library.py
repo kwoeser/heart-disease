e0756cc42791a13fe0a1236d4e32f8549f873602
Karma Woeser <111936122+kwoeser@users.noreply.github.com>
2025-05-11 14:50:49 -0700

    Update library.py

--- library.py
+++ library.py
@@ -13,6 +13,9 @@ import sklearn
 sklearn.set_config(transform_output="pandas")  #says pass pandas tables through pipeline instead of numpy matrices
 import warnings
 
+titanic_variance_based_split = 107   #add to your library
+customer_variance_based_split = 113  #add to your library
+
 
 class CustomMappingTransformer(BaseEstimator, TransformerMixin):
     """
@@ -680,8 +683,6 @@ class CustomTargetTransformer(BaseEstimator, TransformerMixin):
         """
         return self.fit(X, y).transform(X)
 
-titanic_variance_based_split = 107   #add to your library
-customer_variance_based_split = 113  #add to your library
 
 def find_random_state(
     features_df: pd.DataFrame,
@@ -754,6 +755,26 @@ def find_random_state(
 
     return rs_value, Var
 
+def dataset_setup(original_table, label_column_name:str, the_transformer, rs, ts=.2):
+  #your code below
+  features = original_table.drop(columns=[label_column_name]) 
+  labels = original_table[label_column_name].to_list()
+
+  X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=ts, shuffle=True, random_state=rs, stratify=labels)
+  X_train_transformed = the_transformer.fit_transform(X_train, y_train)
+  X_test_transformed = the_transformer.transform(X_test)
+  
+  x_train_numpy = X_train_transformed.to_numpy()
+  x_test_numpy = X_test_transformed.to_numpy()
+  y_train_numpy = np.array(y_train)
+  y_test_numpy = np.array(y_test)
+  return (x_train_numpy, x_test_numpy, y_train_numpy, y_test_numpy)
+
+def titanic_setup(titanic_table, transformer=titanic_transformer, rs=titanic_variance_based_split, ts=.2):
+    return dataset_setup(titanic_table, 'Survived', transformer, rs, ts)
+
+def customer_setup(customer_table, transformer=customer_transformer, rs=customer_variance_based_split, ts=.2):
+    return dataset_setup(customer_table, 'Rating', transformer, rs, ts)
 
 
 titanic_transformer = Pipeline(steps=[
